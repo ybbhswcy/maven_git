b3d811f86609abab5910f262afc7f93c495d1567
郭松 <guosong@woxinni.com.cn>
2020-06-10 20:36:08 +0800

    11

--- src/main/java/com/maven/git/HelloGit.java
+++ src/main/java/com/maven/git/HelloGit.java
@@ -2,20 +2,6 @@ package com.maven.git;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("第一次提交本地库");
-		System.out.println("第二次提交本地库");
-		System.out.println("分支第一次提交");
-		System.out.println("主线提交1");
-		System.out.println("dev提交1");
-		System.out.println("dev提交2");
-		System.out.println("dev提交3");
-		System.out.println("master提交1");
-		System.out.println("master提交2");
-		System.out.println("github在线添加1");
-		
-		System.out.println("dev远程提交");
-		System.out.println("HelloGit_dev远程提交");	
-		
-		System.out.println("github在线添加2");	
+		System.out.println("HelloGit_1");
 	}
 }
--- src/main/java/com/maven/git/HelloGithub.java
+++ src/main/java/com/maven/git/HelloGithub.java
@@ -2,14 +2,6 @@ package com.maven.git;
 
 public class HelloGithub {
 	public static void main(String[] args) {
-		System.out.println("github clone project 添加内容1");
-		System.out.println("github clone project 添加内容2");
-		System.out.println("本地添加内容1");
-		System.out.println("本地添加内容HelloGit提交");
-		System.out.println("mavengit提交1");
-		
-		System.out.println("mavengit_master提交1");
-		
-		System.out.println("mavengit_dev提交____");
+		System.out.println("HelloGit_1");
 	}
 }
