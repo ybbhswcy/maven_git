2151b10b4824898ab1b4a88afba9d67cec2569f9
joe.guo <ybbhswcy@126.com>
2020-06-10 15:57:10 +0800

    github clone project 添加内容2

--- src/main/java/com/maven/git/HelloGithub.java
+++ src/main/java/com/maven/git/HelloGithub.java
@@ -3,5 +3,6 @@ package com.maven.git;
 public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("github clone project 添加内容1");
+		System.out.println("github clone project 添加内容2");
 	}
 }
