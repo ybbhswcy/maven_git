86ace79e7dd6e960586513eb4f7d01e528412dfa
guoyiyang <guosong@163.com>
2020-06-10 15:59:54 +0800

    tijiao2

--- src/main/java/com/maven/git/HelloGithub.java
+++ src/main/java/com/maven/git/HelloGithub.java
@@ -4,5 +4,6 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("github clone project 添加内容1");
 		System.out.println("github clone project 添加内容2");
+		System.out.println("本地添加内容1");
 	}
 }
